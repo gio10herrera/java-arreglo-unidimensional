fd6a276d4b8574b1490cf8a3ece5100f88d2a1cd
Giovan Herrera <gio2020herrera@gmail.com>
2024-10-18 12:50:38 -0500

    agregar dato panel y title panel agregados

--- src/ArregloUnidimensional.java
+++ src/ArregloUnidimensional.java
@@ -1,15 +1,52 @@
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 public class ArregloUnidimensional {
     static JFrame frame;
-    static JPanel panelPadre;
+    static JPanel panelPadre, firstSonPanel, panelTitle;
+    static JTextField txtAddNum;
+    static JButton btnAddData;
+    static JLabel lblTitle;
 
     public static void main(String[] args) {
         inicializarJFrame();
         inicializarComponentes();
         inicializarPanelPadre();
+        inicializarPanelTitle();
+        inicializarFirstSonPanelAddData();
+        addComponentsToPanelTitle();
+        addComponentsToFirstSonPanel();
+        addPanelsToPanelPadre();
         addPanelPadreToFrame();
         frame.setVisible(true);
+    }
+
+    private static void addComponentsToPanelTitle() {
+        panelTitle.add(lblTitle);
+    }
+
+    private static void inicializarPanelTitle() {
+        panelTitle = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    }
+
+    private static void addComponentsToFirstSonPanel() {
+        firstSonPanel.add(txtAddNum);
+        firstSonPanel.add(btnAddData);
+    }
+
+    private static void addPanelsToPanelPadre() {
+        panelPadre.add(panelTitle);
+        panelPadre.add(firstSonPanel);
+    }
+
+    private static void inicializarFirstSonPanelAddData() {
+        firstSonPanel = new JPanel(new GridLayout(1, 2));
+        TitledBorder titledBorder = BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.GRAY), "Agregar Dato");
+        firstSonPanel.setBorder(titledBorder);
     }
 
     private static void addPanelPadreToFrame() {
@@ -22,6 +59,33 @@ public class ArregloUnidimensional {
     }
 
     private static void inicializarComponentes() {
+        lblTitle = new JLabel("Actividad 1");
+        lblTitle.setFont(new Font("Tahoma", Font.BOLD, 14));
+        lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitle.setFocusable(true);
+        txtAddNum = new JTextField();
+        txtAddNum.setHorizontalAlignment(SwingConstants.CENTER);
+        String placeHolder = "Digita el dato";
+        txtAddNum.setText(placeHolder);
+        txtAddNum.setForeground(Color.GRAY);
+        txtAddNum.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent focusEvent) {
+                if (txtAddNum.getText().equals(placeHolder)) {
+                    txtAddNum.setText("");
+                    txtAddNum.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent focusEvent) {
+                if (txtAddNum.getText().isEmpty()) {
+                    txtAddNum.setForeground(Color.GRAY);
+                    txtAddNum.setText(placeHolder);
+                }
+            }
+        });
+        btnAddData = new JButton("Agregar Dato");
     }
 
     private static void inicializarJFrame() {
